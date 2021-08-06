a532b8badebf96e2453ab3528e74505074858d41
Daniel Waite <danielwaite6@hotmail.com>
2021-08-06 10:15:35 -0300

    rentx api running

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -5,9 +5,18 @@ class CategoriesRepository implements ICategoriesRepository {
 
     private categories: Category[];
 
-    constructor() {
+    private static INSTANCE: CategoriesRepository;
+
+    private constructor() {
         this.categories = [];
     };
+
+    public static getInstance(): CategoriesRepository {
+        if (!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+        return CategoriesRepository.INSTANCE;
+    }
 
     create({ name, description }: ICreateCategory): void {
         const category = new Category();
---
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -0,0 +1,13 @@
+import { ListCategoriesController } from "./ListCategoriesController";
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+
+
+const categoriesRepository = CategoriesRepository.getInstance();
+const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+const listCategoriesController = new ListCategoriesController(listCategoriesUseCase)
+
+
+
+export { listCategoriesController, listCategoriesUseCase }
