c746524c218ca1af301d255bff7dafe3f896205d
Daniel Waite <danielwaite6@hotmail.com>
2021-08-06 01:09:19 -0300

    rentx api running

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,25 @@
 import { Router } from 'express';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
-
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
 
-    console.log("name, description: ", name, description);
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-
-    categories.push({
-        name,
-        description,
-    });
+    createCategoryService.execute({ name, description });
 
     return response.status(201).send();
 });
+
+categoriesRoutes.get('/', (request, response) => {
+    const listCategories = categoriesRepository.list();
+    return response.json(listCategories)
+});
+
 
 export { categoriesRoutes };
 
