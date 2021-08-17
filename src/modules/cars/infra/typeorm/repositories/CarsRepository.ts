2a8995b832fc06c9bb9018a74f32c83f08f60bfd
Daniel Waite <danielwaite6@hotmail.com>
2021-08-17 02:00:23 -0300

    first commit

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -40,6 +40,30 @@ class CarsRepository implements ICarsRepository {
     async findByLicensePlate(license_plate: string): Promise<Car> {
         const cars = await this.repository.findOne({ license_plate });
         return cars;
+    };
+
+
+    async findAvailable(name?: string, brand?: string, category_id?: string): Promise<Car[]> {
+
+        const carsQuery = this.repository
+            .createQueryBuilder("c")
+            .where("available = :available", { available: true });
+
+        if (brand) {
+            carsQuery.andWhere("c.brand = :brand", { brand });
+        };
+
+        if (name) {
+            carsQuery.andWhere("c.name = :name", { name });
+        };
+
+        if (category_id) {
+            carsQuery.andWhere("c.category_id = :category_id", { category_id });
+        };
+
+        const cars = await carsQuery.getMany();
+
+        return cars;
     }
 
 };
