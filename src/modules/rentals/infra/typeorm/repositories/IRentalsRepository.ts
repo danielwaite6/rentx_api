f1931c92cb5ffa38a4deba824e8e9950c7c23932
Daniel Waite <danielwaite6@hotmail.com>
2021-08-25 21:41:39 -0300

    other commit

---
+++ src/modules/rentals/infra/typeorm/repositories/IRentalsRepository.ts
@@ -0,0 +1,9 @@
+import { ICreateRentalDTO } from "../../../dtos/ICreateRentalDTO";
+import { Rental } from "../entities/Rental";
+
+interface IRentalsRepository {
+    findOpenRentalByUser(user_id: string): Promise<Rental>;
+    findOpenRentalByCar(car_id: string): Promise<Rental>;
+    create(data: ICreateRentalDTO): Promise<Rental>;
+}
+export { IRentalsRepository };
